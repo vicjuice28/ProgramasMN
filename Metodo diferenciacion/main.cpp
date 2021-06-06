2e715451e11a7111861cae604396217094335b87
Victor Hugo Estrada Ortega <victorhugofp11@hotmail.com>
2021-06-06 14:13:53 -0600

    Metodo diferenciacion funcionando al 100%

--- Metodo diferenciacion/main.cpp
+++ Metodo diferenciacion/main.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 using namespace std;
 
-
+double iteracion, paso, trespuntos, centrada, cincopuntos;
 
 
 
 void Metodo(){
 
-    cout<<"Hola mundo"<<endl;
+    cout<<"Ingrese el numero de iteraciones que desea: "; cin>>iteracion;
+    cout<<"\nIngrese el paso que desea: "; cin>>paso;
+    vector<double>y(iteracion);
+    
+    //Valores en y
+    for(int i=0; i<iteracion; i+=paso){
+        cout<<"Valor en y"<<"["<<i<<"]"<<": "; cin>>y[i];
+          
+    }
+    //Imprimir tabla
+    cout<<"X\tY"<<endl;
+    for (int j=0; j <iteracion; j+=paso)
+        {
+            cout<<j<<"\t"<<y[j]<<endl;
+        }
+
+    //De 3 puntos
+    trespuntos = ((-3*y[0])+ (4*y[1])-y[2])/(2*paso);
+    cout<<"\nDe 3 puntos: "<<trespuntos<<endl;
+    centrada = (y[2]-y[0])/2;
+    cout<<"Centrada: "<<centrada<<endl;
+    cincopuntos = (y[0]-(8* y[1])+(8*y[3])-y[4])/12*paso;
+    cout<<"De 5 puntos: "<<cincopuntos<<endl;
+    
+
+
 
 }
 
