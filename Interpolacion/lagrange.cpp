be436317301c2d3a956f1903a726279c34068bca
Victor Hugo Estrada Ortega <victorhugofp11@hotmail.com>
2021-06-15 16:24:39 -0600

    Pequeño detalle corregido

--- Interpolacion/lagrange.cpp
+++ Interpolacion/lagrange.cpp
@@ -9,7 +9,7 @@ float z,w,sum=0,u,d=1;
 int n = 3;
 float x[3] = {1,4,6},
 f[3] = {0,1.386294,1.791759};
-float test = 2.0;
+float prueb = 2.0;
  
 void Lagrange(){
   cout<<"\nInterpolacion por polinomios de Lagrange: "<<endl;
@@ -18,7 +18,7 @@ void Lagrange(){
   for (i = 0; i < n ; i++) {
     for (j = 0; j < n ; j++) {
       if (j != i){
-        u = u*(test - x[j]);
+        u = u*(prueb - x[j]);
     d = d*(x[i] - x[j]);
       }
     }
